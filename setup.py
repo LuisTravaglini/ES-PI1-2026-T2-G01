6b63421478c3980ffe8e43be8cabf2c8ac6f48da
Luis Travaglini <lftravaglini@gmail.com>
2026-04-23 09:22:20 -0300

    setup2

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 def menu():
     print("\n=== URNA ELETRÔNICA ===")
-    print("1 - Votar")
-    print("2 - Ver resultados")
-    print("3 - Sair")
-
-votos = {
-    "Branco": 0,
-    "Nulo": 0,
-    "Candidato_A": 0,
-    "Candidato_B": 0,
-    "Candidato_C": 0
-}
+    print("1 - Gerenciamento")
+    print("2 - Votação")
 
 while True:
     menu()
@@ -18,37 +9,66 @@ while True:
 
     match opcao:
         case "1":
-            print("Digite seu voto:")
-            print("0 - Branco")
-            print("1 - Nulo")
-            print("2 - Candidato A")
-            print("3 - Candidato B")
-            print("4 - Candidato C")
+            print("=== OPÇÕES DE GERENCIAMENTO ===")
+            print("1 - Candidato.")
+            print("2 - Eleitor")
 
-            voto =  input("Seu voto: ")
+            gerenciamento = input("Sua opção de gerenciamento : ")
 
-            match voto:
-                case "0":
-                    votos["Branco"] =+ 1
-                    print("Voto branco confirmado!")
+            match gerenciamento:
                 case "1":
-                    votos["Nulo"] =+ 1
-                    print("Voto nulo confirmado!")
+                    print("=== OPÇÕES DO CANDIDATO ===")
+                    print("1 - Cadastrar Candidato")
+                    print("2 - Editar informações")
+                    print("3 - Excluir Candidato")
+                    print("4 - voltar") 
+
+                    candidato = input("Selecione uma opção: ")
+
+                    match candidato:
+                        case 1:
+                            print("")
+                        case 2:
+                            print("")
+                        case 3:
+                            print("")
+                        case 4: 
+                            menu()
+
                 case "2":
-                    votos["Candidato_A"] =+ 1
-                    print("Voto no Candidato A confirmado!")
-                case "3":
-                    votos["Candidato_B"] =+ 1
-                    print("Voto no Candidato B confirmado!")
-                case "4":
-                    votos["CandidatoC"] =+ 1
-                    print("Voto no Confirmado C confirmado!")
-                case _:
-                    print("Voto inválido")
+                    print("=== OPÇÕES DO ELEITOR ===")
+                    print("1 - lista de eleitores")
+                    print("2 - Cadastro(Novo eleitor)")
+                    print("3 - voltar")
+
+                    eleitor = input("Selecione uma opção: ")
+
+                    match eleitor:
+                        case "1":
+                            print("1 - Editar Eleitores")
+                            print("2 - Buscar Eleitor por CPF ou Titulo")
+                        case "2":
+                            Cpf = input("DIgite seu CPF: ")
+
+
+
 
         case "2":
-            print("\n=== RESULTADOS ===")
-            print(votos)
+            print("1 - Listar candidatos")
+            print("2 - Buscar Candidatos")
+            print("3 - Sistema de votação")
+
+            votacao = input("Selecione uma opção: ")
+            match votacao:
+                case "1": 
+                    pass
+                    #lista de candidatos (?)
+                case "2": 
+                    pass
+                    #Pesquisa de candidatos (?)
+                case "3":
+                    pass
+                    
 
         case "3":
             print("Encerrando...")
