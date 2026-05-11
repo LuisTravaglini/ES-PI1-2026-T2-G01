7a4399262ebd8b1473860fa3449bdb5842883449
kaikymello668 <kaiky.mrs@puccampinas.edu.br>
2026-05-11 20:48:27 -0300

    Correção do insert
    Insert funcionou apenas uma vez, e essa atualização corrigiu, estava sendo inserido o id que é auto_incremente

--- sist_votacao.sql
+++ sist_votacao.sql
@@ -1,6 +1,8 @@
-insert into candidato values 
-(0, 16, 'Pumapjl', 'Febre90s'),
-(0, 20, 'Predella', 'Costa Gold'),
-(0, 45, 'Nog', 'Costa Gold'),
-(0, 14, 'Brown', 'Racionais'),
-(0, 11, 'Jean Tassy', 'The gust MCs')
+INSERT INTO candidato
+(numero_Candidato, nome_Completo, partido)
+VALUES
+(16, 'Pumapjl', 'Febre90s'),
+(20, 'Predella', 'Costa Gold'),
+(45, 'Nog', 'Costa Gold'),
+(14, 'Brown', 'Racionais'),
+(11, 'Djonga', 'DV Tribo');
