ecf26abfea30b876ddcdc2489e51fb4d2c52785e
arthur <arthurraymundi@gmail.com>
2026-05-26 21:00:26 -0300

    Corrigindo a main

--- main.py
+++ main.py
@@ -182,8 +182,8 @@ while opcao == 0:
                             cpf = input("Digite os 4 primeiros dígitos do CPF: ")
                             chave = input("Digite sua chave de acesso: ")
 
-                            cpf_criptografado = criptografar(cpf)
-                            chave_criptografado = criptografar(chave)
+                            cpf_criptografado = criptografar_cpf(cpf)
+                            chave_criptografado = criptografar_chave(chave)
 
                             # CONSULTA DADOS DO ELEITOR
                             query = """
@@ -234,8 +234,8 @@ while opcao == 0:
                             cpf = input("Digite os 4 primeiros dígitos do CPF: ")
                             chave = input("Digite sua chave de acesso: ")
                             
-                            cpf_criptografado = criptografar(cpf)
-                            chave_criptografado = criptografar(chave)
+                            cpf_criptografado = criptografar_cpf(cpf)
+                            chave_criptografado = criptografar_chave(chave)
 
                             # CONSULTA DADOS DO MESÁRIO
                             query = """
